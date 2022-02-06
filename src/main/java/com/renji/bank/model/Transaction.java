9ca4f16887f7ecb0f3b8fdc2b322a2979bb4eec7
vrenjithkr <vrenjithkr@gmail.com>
2022-02-06 13:16:12 +0530

    Code update

---
+++ src/main/java/com/renji/bank/model/Transaction.java
@@ -0,0 +1,59 @@
+package com.renji.bank.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transaction {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+
+	long fromAccount;
+	long toAccount;
+	long amount;
+	String status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public long getFromAccount() {
+		return fromAccount;
+	}
+
+	public void setFromAccount(long fromAccount) {
+		this.fromAccount = fromAccount;
+	}
+
+	public long getToAccount() {
+		return toAccount;
+	}
+
+	public void setToAccount(long toAccount) {
+		this.toAccount = toAccount;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
--- src/main/java/com/renji/transfer/controller/TransferController.java
+++ src/main/java/com/renji/transfer/controller/TransferController.java
@@ -3,17 +3,20 @@ package com.renji.transfer.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.renji.bank.model.Transaction;
 
 @RestController
 @RequestMapping("/api/v1/transfer")
 public class TransferController {
 
 	@PostMapping
-	public ResponseEntity<String> doTransafer() {
-		return new ResponseEntity<>("Transaction is initiated", HttpStatus.OK);
+	public ResponseEntity<Transaction> doTransafer(@RequestBody Transaction transaction) {
+		transaction.setStatus("Transaction is initiated");
+		return new ResponseEntity<Transaction>(transaction, HttpStatus.OK);
 	}
 
 
